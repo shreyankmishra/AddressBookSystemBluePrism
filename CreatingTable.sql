7efcdccf2fb763924e998ad92ba26e38d3b37a0f
shreyankmishra <mishra.shreyank@gmail.com>
2020-11-16 20:29:39 +0530

    Add: Creating Table in Address Book System

---
+++ CreatingTable.sql
@@ -0,0 +1,11 @@
+create table AddressBook
+(
+    first_name varchar(50),
+    last_name varchar(50),
+    address varchar(100),
+    city varchar(50),
+    state varchar(50),
+    zip int,
+    phone_number varchar(10),
+    email varchar(50)
+);
